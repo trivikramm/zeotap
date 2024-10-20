42e718e8aa0256b4cb2633258a29bf678f7a47e4
trivikramm <76730493+trivikramm@users.noreply.github.com>
2024-10-20 20:44:08 +0530

    Create node.py

---
+++ rule_engine/src/node.py
@@ -0,0 +1,6 @@
+class Node:
+    def __init__(self, type, left=None, right=None, value=None):
+        self.type = type
+        self.left = left
+        self.right = right
+        self.value = value
