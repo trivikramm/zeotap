ad34e5cf6e69afd0a20ffbe631d48e2833762853
trivikramm <76730493+trivikramm@users.noreply.github.com>
2024-10-20 20:33:52 +0530

    Create __init__.py

---
+++ weather_monitoring/src/__init__.py
@@ -0,0 +1,7 @@
+# This file can be left empty or used to initialize the package
+
+#### `src/config.py`
+
+API_KEY = 'b1fcdd11dabd1065e7fa2353536ee9ed'
+METROS = ['Delhi', 'Mumbai', 'Chennai', 'Bangalore', 'Kolkata', 'Hyderabad']
+INTERVAL = 300  # 5 minutes in seconds
